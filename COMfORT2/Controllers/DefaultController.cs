409d3b6d59eab174975821f315df6f08046c34ac
Tom Smale <smaletho@gmail.com>
2018-08-22 10:28:12 -0400

    continued dev

--- COMfORT2/Controllers/DefaultController.cs
+++ COMfORT2/Controllers/DefaultController.cs
@@ -59,8 +59,8 @@ namespace COMfORT2.Controllers
 
             using (ZipFile zip = new ZipFile())
             {
-                zip.AddDirectory(Server.MapPath("~/Content"));
-
+                zip.AddDirectory(Server.MapPath("~/Content/"), "/Content/");
+                
                 // Add Index.html
                 //  I need to strip down this file, and include the variables "config1.js" and config2.js"
 
@@ -68,52 +68,56 @@ namespace COMfORT2.Controllers
                 {
                     string text = System.IO.File.ReadAllText(Server.MapPath("~/Views/View/Index.cshtml"));
 
-                    // replace <link href="~/Content/css/base.css" rel="stylesheet" />
-                    //  with <link href="~/css/base.css" rel="stylesheet" />
-                    text = text.Replace("<link href=\"~/Content/css/base.css\" rel=\"stylesheet\" />", "<link href=\"./css/base.css\" rel=\"stylesheet\" />");
+                    text = text.Replace("~/Content", "./Content");
 
-                    // replace <link href="~/Content/css/pageContent.css" rel="stylesheet" />
-                    //  with <link href="~/css/pageContent.css" rel="stylesheet" />
-                    text = text.Replace("<link href=\"~/Content/css/pageContent.css\" rel=\"stylesheet\" />", "<link href=\"./css/pageContent.css\" rel=\"stylesheet\" />");
+                    //// replace <link href="~/Content/css/base.css" rel="stylesheet" />
+                    ////  with <link href="~/css/base.css" rel="stylesheet" />
+                    //text = text.Replace("<link href=\"~/Content/css/base.css\" rel=\"stylesheet\" />", "<link href=\"./css/base.css\" rel=\"stylesheet\" />");
 
-                    // replace <script src="~/Content/js/jquery-3.3.1.min.js"></script>
-                    //  with <script src="~/js/jquery-3.3.1.min.js"></script>
-                    text = text.Replace("<script src=\"~/Content/js/jquery-3.3.1.min.js\"></script>", "<script src=\"./js/jquery-3.3.1.min.js\"></script>");
+                    //// replace <link href="~/Content/css/pageContent.css" rel="stylesheet" />
+                    ////  with <link href="~/css/pageContent.css" rel="stylesheet" />
+                    //text = text.Replace("<link href=\"~/Content/css/pageContent.css\" rel=\"stylesheet\" />", "<link href=\"./css/pageContent.css\" rel=\"stylesheet\" />");
 
-                    // replace <script src="~/Content/js/navigation.js"></script>
-                    //  with <script src="~/js/navigation.js"></script>
-                    text = text.Replace("<script src=\"~/Content/js/navigation.js\"></script>", "<script src=\"./js/navigation.js\"></script>");
+                    //// replace <script src="~/Content/js/jquery-3.3.1.min.js"></script>
+                    ////  with <script src="~/js/jquery-3.3.1.min.js"></script>
+                    //text = text.Replace("<script src=\"~/Content/js/jquery-3.3.1.min.js\"></script>", "<script src=\"./js/jquery-3.3.1.min.js\"></script>");
 
-                    // replace <script src="~/Content/js/base.js"></script>
-                    //  with <script src="~/js/base.js"></script>
-                    text = text.Replace("<script src=\"~/Content/js/base.js\"></script>", "<script src=\"./js/base.js\"></script>");
+                    //// replace <script src="~/Content/js/navigation.js"></script>
+                    ////  with <script src="~/js/navigation.js"></script>
+                    //text = text.Replace("<script src=\"~/Content/js/navigation.js\"></script>", "<script src=\"./js/navigation.js\"></script>");
 
-                    // replace <script src="~/Content/js/validate.js"></script>
-                    //  with <script src="~/js/validate.js"></script>
-                    text = text.Replace("<script src=\"~/Content/js/validate.js\"></script>", "<script src=\"./js/validate.js\"></script>");
+                    //// replace <script src="~/Content/js/base.js"></script>
+                    ////  with <script src="~/js/base.js"></script>
+                    //text = text.Replace("<script src=\"~/Content/js/base.js\"></script>", "<script src=\"./js/base.js\"></script>");
 
-                    // replace <script src="~/Content/js/loading.js"></script>
-                    //  with <script src="~/js/loading.js"></script>
-                    text = text.Replace("<script src=\"~/Content/js/loading.js\"></script>", "<script src=\"./js/loading.js\"></script>");
+                    //// replace <script src="~/Content/js/validate.js"></script>
+                    ////  with <script src="~/js/validate.js"></script>
+                    //text = text.Replace("<script src=\"~/Content/js/validate.js\"></script>", "<script src=\"./js/validate.js\"></script>");
 
-                    // replace <script src="~/Content/js/shared.js"></script>
-                    //  with <script src="~/js/shared.js"></script>
-                    text = text.Replace("<script src=\"~/Content/js/shared.js\"></script>", "<script src=\"./js/shared.js\"></script>");
+                    //// replace <script src="~/Content/js/loading.js"></script>
+                    ////  with <script src="~/js/loading.js"></script>
+                    //text = text.Replace("<script src=\"~/Content/js/loading.js\"></script>", "<script src=\"./js/loading.js\"></script>");
 
-                    // replace <script src="~/Content/js/userTrack.js"></script>
-                    //  with <script src="~/js/userTrack.js"></script>
-                    text = text.Replace("<script src=\"~/Content/js/userTrack.js\"></script>", "<script src=\"./js/userTrack.js\"></script>");
+                    //// replace <script src="~/Content/js/shared.js"></script>
+                    ////  with <script src="~/js/shared.js"></script>
+                    //text = text.Replace("<script src=\"~/Content/js/shared.js\"></script>", "<script src=\"./js/shared.js\"></script>");
 
-                    // ---- Special case: replace offline.js with both config files (created next)
-                    // replace <script src="~/Content/js/offline.js"></script>
-                    //  with <script src="~/js/offline.js"></script><script src="~/js/config1.js"></script><script src="~/js/config2.js"></script>
+                    //// replace <script src="~/Content/js/userTrack.js"></script>
+                    ////  with <script src="~/js/userTrack.js"></script>
+                    //text = text.Replace("<script src=\"~/Content/js/userTrack.js\"></script>", "<script src=\"./js/userTrack.js\"></script>");
+
+                    //// ---- Special case: replace offline.js with both config files (created next)
+                    //// replace <script src="~/Content/js/offline.js"></script>
+                    ////  with <script src="~/js/offline.js"></script><script src="~/js/config1.js"></script><script src="~/js/config2.js"></script>
+
+                    string scriptText = "<script src=\"./Content/js/offline.js\"></script>";
+                    scriptText += "<script src=\"./Content/js/config1.js\"></script>";
+                    scriptText += "<script src=\"./Content/js/config2.js\"></script>";
+                    text = text.Replace("<script src=\"./Content/js/offline.js\"></script>", scriptText);
 
 
-                    text = text.Replace("<script src=\"~/Content/js/offline.js\"></script>", "<script src=\"./js/offline.js\"></script><script src=\"./js/config1.js\"></script><script src=\"./js/config2.js\"></script>");
-
-
-                    // replace loading image
-                    text = text.Replace("<img src=\"~/Content/images/loading.gif\" />", "<img src=\"./images/loading.gif\" />");
+                    //// replace loading image
+                    ///text = text.Replace("<img src=\"~/Content/images/loading.gif\" />", "<img src=\"./images/loading.gif\" />");
                     
                     tw.Write(text);
                     zip.AddFile(homeFile).FileName = "index.html";
@@ -127,7 +131,7 @@ namespace COMfORT2.Controllers
                     tw.Write(model.ConfigXml.OuterXml);
                     tw.Write("`);");
                     
-                    zip.AddFile(configFile1).FileName = "/js/config1.js";
+                    zip.AddFile(configFile1).FileName = "/Content/js/config1.js";
                 }
 
                 using (var tw = new StreamWriter(configFile2, true))
@@ -155,7 +159,7 @@ namespace COMfORT2.Controllers
 
                     tw.WriteLine("}");
 
-                    zip.AddFile(configFile2).FileName = "/js/config2.js";
+                    zip.AddFile(configFile2).FileName = "/Content/js/config2.js";
                 }
                 
 
---
+++ COMfORT2/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace COMfORT2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<COMfORT2.ComfortModel>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(COMfORT2.ComfortModel context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
