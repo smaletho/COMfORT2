43844a3c60ead752709fdfeca063e1f2fade0186
Ryan Kinnen <ryankinnen@gmail.com>
2018-09-10 16:37:35 -0400

    ManagementPages: Menu page complete

---
+++ COMfORT2/Content/js/Management/MainMenu.js
@@ -0,0 +1,14 @@
+﻿pageInit();
+
+var navigateToUrl = '';
+
+function pageInit() {
+    $('menu-item').click(function () {
+        $(this).addClass('menu-item-select');
+        navigateToUrl = $(this).data('url');
+        setTimeout(
+            function () {
+                window.location.href = navigateToUrl;
+            }, 250);
+    });
+}
